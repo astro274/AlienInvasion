66709c49593499a421e06b1172daf6182848c6a3
astro274 <adnanmohamed922@gmail.com>
2023-08-28 07:07:43 +0530

    Fixed a typo in the main .py file

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,7 +9,7 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((self.screen.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
         # Set the background color.
         self.bg_color = (230, 230, 230)
